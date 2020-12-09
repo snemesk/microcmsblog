fec6a3c7c36f3f10f9eb7b8227788d2ca7a4b9dd
snemesk <sainemesis.ksk@gmail.com>
2020-12-10 03:57:33 +0900

    tes8

--- pages/index.vue
+++ pages/index.vue
@@ -37,6 +37,10 @@
     </li>
   </ul>
   </section>
+  <section>
+  <a class="twitter-timeline" href="https://twitter.com/obisuki_food?ref_src=twsrc%5Etfw">Tweets by obisuki_food</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8">
+  </script>
+  </section>
 </template>
 
 <script>
