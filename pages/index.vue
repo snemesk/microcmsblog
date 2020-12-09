da369ae0250293ac316a8923480a61421236e0c3
snemesk <sainemesis.ksk@gmail.com>
2020-12-10 00:45:37 +0900

    tes3

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,4 @@
 <template>
-<section class="bar background-image-fixed-2 no-mb color-white text-center">
-    <div class="dark-mask"></div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <!--<div class="icon icon-lg"><i class="{{ .Site.Params.see_more.icon }}"></i>
-                </div>-->
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <h3>{{ .Site.Params.see_more.title | markdownify }}</h3>
-                <p class="lead">{{ .Site.Params.see_more.subtitle | markdownify }}</p>
-                <p class="text-center">
-                    {{ with .Site.Params.see_more.link_url }}
-                     <a href="{{ . }}" class="btn btn-template-transparent-black btn-lg">
-                    {{ end }}
-                        {{ with .Site.Params.see_more.link_text }}
-                        {{ . | markdownify }}
-                        {{ end }}
-                    {{ with .Site.Params.see_more.link_url }}
-                    </a>
-                    {{ end }}
-                </p>
-                <br>
-                <br><br>
-                <br>
-                <br>
-            </div>
-        </div>
-    </div>
-</section>
-
 <div class="contents">
   <ul>
     <li v-for="content in contents" :key="content.id">
