4b7628ef9c388507b0f3dbb71805dd4fc962501f
snemesk <sainemesis.ksk@gmail.com>
2020-12-18 21:46:00 +0900

    fix1

--- components/header.vue
+++ components/header.vue
@@ -4,15 +4,21 @@
         <div class="header-wrapper"><a class="header__logo" href="/">test</a>
           <nav>
             <ul>
-              <li class="nav-item active"><a href="index.html">Home</a>
+              <li class="nav-item active"><a href="/">Home</a>
               </li>
-              <li class="nav-item"><a href="#">Pages</a>
+              <li class="nav-item"><a href="/">Pages</a>
               </li>
               <li class="nav-item"><a href="/about">About</a></li>
             </ul>
           </nav>
           <div class="header__icon-group"><a href="#" id="search"><i class="fas fa-search"></i></a>
-            <div class="social"><a href="#"><i class="fab fa-facebook-f"></i></a><a href="#"><i class="fab fa-twitter"></i></a><a href="#"><i class="fab fa-instagram"></i></a><a href="#"><i class="fab fa-dribbble"></i></a><a id="mobile-menu-controller" href="#"><i class="fas fa-bars"></i></a></div>
+            <div class="social">
+              <a href="#"><i class="fab fa-facebook-f"></i></a>
+              <a href="#"><i class="fab fa-twitter"></i></a>
+              <a href="#"><i class="fab fa-instagram"></i></a>
+              <a href="#"><i class="fab fa-dribbble"></i></a>
+              <a id="mobile-menu-controller" href="#"><i class="fas fa-bars"></i></a>
+              </div>
           </div>
         </div>
       </div>
